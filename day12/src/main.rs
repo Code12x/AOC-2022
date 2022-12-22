e92e337fedfa167439194684ad857075c21a2137
Code12x <87677172+Code12x@users.noreply.github.com>
2022-12-22 15:10:58 -0500

    Progress

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -3,6 +3,13 @@ use std::io::ErrorKind;
 
 const DONE_MARK: char = '*';
 
+#[derive(Debug)]
+enum Operation {
+    Add,
+    Subtract,
+}
+
+#[derive(Debug)]
 #[derive(Clone)]
 struct Poi {
     y: usize,
@@ -27,8 +34,8 @@ impl Map {
         Err(ErrorKind::NotFound)
     }
 
-    fn get_poi(&self, y: usize, x: usize) -> &Poi {
-        &self.map[y][x]
+    fn get_poi(&self, y: usize, x: usize) -> Poi {
+        self.map[y][x].clone()
     }
 
     fn get_possible_moves(&self, poi: &Poi) -> [bool;4] {
@@ -73,8 +80,8 @@ impl Map {
                 
                 // Check if the position to the left of poi is possible
                 3 => {
-                    if poi.y > 0 && (self.get_poi(poi.y, poi.x-1).height as i32 - poi.height as i32 == 1 ||
-                                     self.get_poi(poi.y, poi.x-1).height as i32 - poi.height as i32 == 0) && 
+                    if poi.y > 0 && (self.get_poi(poi.y, (poi.x as i32 - 1) as usize).height as i32 - poi.height as i32 == 1 ||
+                                     self.get_poi(poi.y, (poi.x as i32 - 1) as usize).height as i32 - poi.height as i32 == 0) && 
                         self.get_poi(poi.y, poi.x-1).mark != DONE_MARK {
                         true
                     } else {
@@ -90,6 +97,10 @@ impl Map {
         }
         results
     }
+    
+    fn set_mark(&mut self, y: usize, x: usize, mark: char) {
+        self.map[y][x].mark = mark;
+    }
 }
 
 fn get_input() -> Vec<Vec<Poi>> {
@@ -97,22 +108,22 @@ fn get_input() -> Vec<Vec<Poi>> {
     let mut map = Vec::<Vec<Poi>>::new();
     
     for (y, line) in input.lines().enumerate() {
+        map.push(vec![]);
         for (x, character) in line.chars().enumerate() {
-            map[y][x] = Poi { y, x, height: character, mark: character };
+            map[y].push(Poi { y, x, height: character, mark: character });
         }
     }
     map
 }
 
 #[derive(Clone)]
-struct Path<'a> {
-    poi: &'a Poi,
+#[derive(Debug)]
+struct Path {
+    poi: Poi,
     moves_count: i32,
 }
 
-
 fn main() {
-
     let mut map = Map { map: get_input() };
 
     // Santa
@@ -121,6 +132,7 @@ fn main() {
         Err(e) => panic!("Error finding santa {}", e),
     };
     map.map[santa_cords.0][santa_cords.1].height = 'a';
+    println!("Santa setup {:?}", santa_cords);
 
     // Target
     let target_cords = match map.find_mark('E') {
@@ -128,21 +140,96 @@ fn main() {
         Err(e) => panic!("Error finding target {}", e),
     };
     map.map[target_cords.0][target_cords.1].height = 'z';
+    println!("Target setup {:?}", target_cords);
 
     // Setup the paths and give it the inital value of santa's path from santa_cords
     let mut paths: Vec<Path> = Vec::new();
     paths.push(Path {poi: map.get_poi(santa_cords.0, santa_cords.1), moves_count: 0});
+    println!("Paths setup {:?}", paths);
+    println!("******************************************************************");
     
     // The main loop... go through the paths at the time of the loop and check for new paths and
-    // add them to the paths... if a path reaches the target, output the moves_count
+    // add them to the paths... if a path reaches the target, output the moves_count, else add the
+    // DONE_MARK
     // Somewhere loop through paths and set each one's mark to DONE_MARK
     let mut is_found = false;
     'is_found_loop: while !is_found {
-        let mut current_paths = paths.clone();
+        for path in &paths {
+            map.set_mark(path.poi.y, path.poi.x, DONE_MARK);
+            println!("Set mark DONE_MARK for {:?}", path);
+            println!("{:?}", map.map[20][0]);
+        }
+        let current_paths = paths.clone();
+        println!("current_paths = {:?}", current_paths);
+
         paths.clear();
 
         for current_path in current_paths {
+            println!("current_path: {:?}", current_path);
+            let possible_moves = map.get_possible_moves(&current_path.poi);
+            println!("possible_moves for &current_path.poi: {:?}", &current_path.poi);
 
+            for i in [0 as usize, 1 as usize, 2 as usize, 3 as usize] {
+                let mut y_op: Option<Operation> = None;
+                let mut x_op: Option<Operation> = None;
+
+                match i {
+                    0 => y_op = Some(Operation::Subtract),
+                    1 => x_op = Some(Operation::Add),
+                    2 => y_op = Some(Operation::Add),
+                    3 => x_op = Some(Operation::Subtract),
+                    _ => panic!("Error here and impossible to occur :D"),
+                }
+
+                println!("y_op for possible_moves[{}]: {:?}", i, y_op);
+                println!("x_op for possible_moves[{}]: {:?}", i, x_op);
+
+                if possible_moves[i] {
+                    let y: usize = match y_op {
+                        Some(operation) => {
+                            match operation {
+                                Operation::Add => current_path.poi.y + 1,
+                                Operation::Subtract => current_path.poi.y - 1,
+                            }
+                        },
+                        None => current_path.poi.y,
+                    };
+
+                    let x: usize = match x_op {
+                        Some(operation) => {
+                            match operation {
+                                Operation::Add => current_path.poi.x + 1,
+                                Operation::Subtract => current_path.poi.x - 1,
+                            }
+                        }
+                        None => current_path.poi.x,
+                    };
+
+                    let mut is_accounted_for = false;
+                    for path in &paths {
+                        if x == path.poi.x && y == path.poi.y {
+                            is_accounted_for = true;
+                        }
+                    }
+
+                    println!("is {}, {} accounted for? {}", y, x, is_accounted_for);
+
+                    if !is_accounted_for {
+                        let new_poi = map.get_poi(y, x);
+                        let new_path = Path { poi: new_poi, moves_count: current_path.moves_count+1 };
+                        paths.push(new_path);
+
+                        if y == target_cords.0 && x == target_cords.1 {
+                            is_found = true;
+                            continue 'is_found_loop;
+                        } 
+                    }
+                }
+            }
+            println!("=============================================================");
         }
+        println!("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
     }
+
+    println!("The shortest path is {:?} moves long", paths[paths.len()-1]);
 }
